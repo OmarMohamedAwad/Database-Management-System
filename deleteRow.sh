b3a41d72d4cc01d41ddc453fa1b31cf9dcabe34b
OmarMohamedAwad <omar.awad825@gmail.com>
2021-01-25 20:14:11 +0200

    edit delete line

--- deleteRow.sh
+++ deleteRow.sh
@@ -1,19 +1,46 @@
 #!/bin/bash
 
+function checkCoulmnValue 
+{
+	echo -e "Enter Column Value: \c"
+	read columnValue
+	typeset arrayRows[2]
+	index=0
+
+	columnVal=`awk 'BEGIN{FS=","}{for(i=1;i<=NF;i++){if($i=="'$columnName'"){j=i+1; if($j == "'$columnValue'"){print NR}}}}' databases/$currentDb/$tbName`
+	for i in $columnVal
+	do
+		arrayRows[$index]=$i
+		(( index+=1 ))
+	done
+
+	(( index=1 ))
+	line=""
+	for i in ${arrayRows[*]}
+	do
+		if [ $index -eq ${#arrayRows[*]} ]
+		then
+			line+="${i}d" 
+		else
+			line+="${i}d;"
+		fi
+		(( index+=1 ))
+	done
+	`sed -i $line databases/$currentDb/$tbName`
+}
+
 function checkColumn
 {
         echo -e "Enter Column Name: \c"
 		read columnName
 		#Chenge it when merge |
-		column=`cut -f2- -d, databases/$currentDb/${tbName}_Schema | grep ,$columnName,`
-		if [ -z $column ]
+		column=$(awk 'BEGIN{FS=","}{for(i=1;i<=NF;i++){if($i=="'$columnName'") print $i}}' databases/$currentDb/${tbName}_Schema)
+		if [[ $column = "" ]]
 		then
 			echo "Invalid Column"
 			checkColumn
 		else
-			echo -e "Enter Column Value: \c"
-			read columnValue
-			`sed -i '/',$columnName,$columnValue,'/d' databases/$currentDb/$tbName`
+			checkCoulmnValue
 			echo "All data have this value are deleted"
 		fi
 }
