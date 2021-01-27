06914573a702158fdc44ff77025327611778a2ac
OmarMohamedAwad <omar.awad825@gmail.com>
2021-01-27 22:39:45 +0200

    Update Data Row

--- updateTable.sh
+++ updateTable.sh
@@ -21,7 +21,7 @@ function update
 					exit
 				;;
 				2)
-					updateRow
+					updateRowData
 					exit
 				;;
 				3)
@@ -118,9 +118,9 @@ function updateDBSchema
 
 function updateRows
 {
-	# sed -i "/$tbName,/d" "databases/$currentDb/Schema"
 	dataLoopCounter=`awk -F, '{ if(NR == 1)print NF }' databases/$currentDb/$tbName`
 	dataLinesCounter=`cat databases/$currentDb/$tbName | wc -l`
+	
 	typeset dataFieldsArray[2]
 	for ((line=0;line<"$dataLinesCounter";line++))
 	do 
@@ -162,5 +162,71 @@ function updateRows
 		done
 	done 
 }
+
+function updateRowData 
+{
+	echo -e "Enter Column Name: \c"
+	read columnName
+
+	echo -e "Enter Previous Column Value: \c"
+	read previousValue
+
+	echo -e "Enter New Column Value: \c"
+	read newValue
+	
+	typeset arrayRows[2]
+	index=0
+	isPrimaryKey=0
+
+	fieldLoopCounter=`awk -F, '{ print NF }' databases/$currentDb/${tbName}_Schema `
+	columnLines=`awk 'BEGIN{FS=","}{for(i=1;i<=NF;i++){if($i=="'$columnName'"){j=i+1; if($j == "'$previousValue'"){print NR}}}}' databases/$currentDb/$tbName`
+	columnDataType=`awk 'BEGIN{FS=","}{for(i=1;i<=NF;i++){if($i=="'$columnName'"){j=i+1; {print $j; i=NF+1}}}}' databases/$currentDb/${tbName}_Schema`
+	typeset fieldsArray[2]
+	((fieldCounter=1))
+	((arrayCounter=0))
+	for ((j=0;j<"$fieldLoopCounter";j++));do
+		i=`cat databases/$currentDb/${tbName}_Schema | cut -f$fieldCounter -d,`
+		if [ $i != "int" -a $i != "varchar" -a $i != "string" ]
+		then 
+			fieldsArray[$arrayCounter]=$i
+			((arrayCounter=arrayCounter+1))
+		fi
+		((fieldCounter=fieldCounter+1))
+	done
+	
+	if [[ -n $columnLines ]]
+	then	
+		if [ ${fieldsArray[((arrayCounter-1))]} = $columnName ]
+		then
+			((isPrimaryKey=1)) 
+			for i in `awk -F, '{print $NF}' databases/$currentDb/$tbName`
+			do
+				if [ $newValue = $i ]
+				then
+					echo "Error, primary key exist"
+					return
+				fi
+			done
+		fi
+
+
+		source ./dataType.sh "checkUserInput" $newValue $columnDataType
+		if [ $userInputDatatype -eq 1 ]
+    	then
+			if [ $isPrimaryKey -eq 1 ]
+			then
+			    sed -i "s/$columnName,$previousValue/$columnName,$newValue/g" "databases/$currentDb/$tbName" 
+			else
+				sed -i "s/,$columnName,$previousValue,/,$columnName,$newValue,/g" "databases/$currentDb/$tbName" 
+			fi
+			echo "Value/Values updated successfully"
+    	else
+        	echo "Error, wrong datatype"
+    	fi 
+	else
+		echo "You Enter Invalid Column Or Data"
+	fi		
+}
+
 
 update
